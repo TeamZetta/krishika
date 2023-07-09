9f712e670f4189d858c703fd14ae5e3b3f7a0555
Diptanshu Mahish <diptanshumahish2016@gmail.com>
2023-07-09 22:52:32 +0530

    add

--- client/src/components/Home/Forum.jsx
+++ client/src/components/Home/Forum.jsx
@@ -9,8 +9,8 @@ export default function Forum({ params }) {
   const { token } = useContext(AppContext);
   const getPosts = async () => {
     const response = await getForum(token);
-    setForums(response.data);
     console.log(response.data);
+    setForums(response.data);
   };
 
   useEffect(() => {
@@ -24,24 +24,28 @@ export default function Forum({ params }) {
 
       <h4 className="py-2 font-bold">Forum Posts</h4>
       <div className="flex flex-col gap-2 py-4">
-        {forums.map((ele) => {
-          return (
-            <div
-              key={ele._id}
-              className="bg-white flex flex-col p-4 rounded-md shadow-sm"
-            >
-              <div className="flex flex-col ">
-                <span className="font-bold">{ele.author.fullName}</span>
-                <span className="text-xs">{ele.author.role}</span>
-              </div>
-              <div className="py-2 border-b border-border-light">
-                {ele.thread}
-              </div>
+        {forums.length !== 0 ? (
+          forums.map((ele) => {
+            return (
+              <div
+                key={ele._id}
+                className="bg-white flex flex-col p-4 rounded-md shadow-sm"
+              >
+                <div className="flex flex-col ">
+                  <span className="font-bold">{ele.author.fullName}</span>
+                  <span className="text-xs">{ele.author.role}</span>
+                </div>
+                <div className="py-2 border-b border-border-light">
+                  {ele.thread}
+                </div>
 
-              <div>comments {ele.comments.length}</div>
-            </div>
-          );
-        })}
+                <div className="pt-2">replies ({ele.comments.length})</div>
+              </div>
+            );
+          })
+        ) : (
+          <div>loading</div>
+        )}
       </div>
     </div>
   );
