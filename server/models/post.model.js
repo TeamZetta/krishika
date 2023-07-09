66a7af0016f59ac3dce3572d708e306d165e3779
ABHIRUP <abhirupsaha02@gmail.com>
2023-07-09 17:42:13 +0530

    feed and forum handle

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,24 +1,35 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema(
-  {
-    content: { type: String, required: true },
-    postImg: { type: String },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-    likes: { type: Number, default: 0 },
-  },
-  {
-    timestamps: true,
-    toJSON: {
-      transform(doc, ret) {
-        delete ret.createdAt
-        delete ret.updatedAt
-        delete ret.__v
-      }
+const postSchema = new mongoose.Schema({
+    thread: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+    status: {
+        type: String,
+        enum: ['FEED', 'FORUM']
     }
-  }
+},
+    {
+        timestamps: true,
+        toJSON: {
+            transform(doc, ret) {
+                delete ret.createdAt
+                delete ret.updatedAt
+                delete ret.__v
+            }
+        }
+    }
 )
 
-const Post = mongoose.model("Post", postSchema)
+const Post = mongoose.model('Post', postSchema)
 
 module.exports = Post
