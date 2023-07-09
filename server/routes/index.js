db98f7681cdf9c0dbbe70d28decf0ae6e2ebb80f
r3yc0n1c <raja.majumdar.01@gmail.com>
2023-07-09 14:29:46 +0530

    feat: server arc, auth, sms, models

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const AuthController = require("../controller/auth.controller")
 
 // Test route
-router.get('/', (req, res) => {
-  return res.status(200).json({ message: 'I am the test-route!' })
+router.get('/api/v1', (req, res) => {
+  return res.status(200).json({ message: 'I am the test route!' })
 })
+
+// Auth routes
+router.post("/api/v1/signup", AuthController.signup)
+router.post("/api/v1/login", AuthController.login)
 
 module.exports = router
