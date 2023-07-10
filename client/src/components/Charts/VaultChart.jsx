6477d480957f4abee5287ed0abd152d8280c0ed5
r3yc0n1c <raja.majumdar.01@gmail.com>
2023-07-10 11:10:46 +0530

    feat: Charts

---
+++ client/src/components/Charts/VaultChart.jsx
@@ -0,0 +1,86 @@
+import { ResponsivePie } from '@nivo/pie'
+
+const margin = { top: 40, right: 200, bottom: 30, left: 30 };
+
+const styles = {
+    root: {
+      fontFamily: "consolas, sans-serif",
+      textAlign: "center",
+      position: "relative",
+      width: 530,
+      height: 400
+    },
+    overlay: {
+      position: "absolute",
+      top: 0,
+      right: margin.right,
+      bottom: 0,
+      left: margin.left,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: 70,
+      color: "#000",
+      // background: "#FFFFFF33",
+      textAlign: "center",
+      // This is important to preserve the chart interactivity
+      pointerEvents: "none"
+    },
+    totalLabel: {
+      fontSize: 24
+    }
+  };
+
+const VaultChart = ({ data, centerText }) => {
+  return (
+    <div style={styles.root}>
+      <ResponsivePie
+        data={data}
+        margin={margin}
+        innerRadius={0.8}        
+        colors={{ scheme: 'category10' }}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+        enableArcLabels={false}
+        enableArcLinkLabels={false}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextColor="#333333"
+        radialLabelsLinkColor={{ from: 'color' }}
+        sliceLabelsSkipAngle={10}
+        sliceLabelsTextColor="#333333"
+        legends={[
+            {
+                anchor: 'bottom',
+                direction: 'row',
+                justify: false,
+                translateX: 40,
+                translateY: 34,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: '#999',
+                itemDirection: 'left-to-right',
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000'
+                        }
+                    }
+                ]
+            }
+        ]}
+      />
+    <div style={styles.overlay}>
+      <span> {centerText} </span>
+      <span style={styles.totalLabel}></span>
+    </div>
+    </div>
+  );
+};
+
+export default VaultChart;
--- client/src/components/Home/Dashboard.jsx
+++ client/src/components/Home/Dashboard.jsx
@@ -6,28 +6,92 @@ import useSWR from "swr";
 import { getNearestMandi } from "../../../packages/api-management/getNearestMandi";
 import { ClipLoader } from "react-spinners";
 import { AppContext } from "@/context/ContextProvider";
+import PieChart from "../Charts/PieChart";
+import VaultChart from "../Charts/VaultChart";
+
+const data = [
+  {
+    id: "Required",
+    label: "sass",
+    value: 99,
+    color: "hsl(87, 70%, 50%)",
+  },
+  {
+    id: "python",
+    label: "python",
+    value: 490,
+    color: "hsl(173, 70%, 50%)",
+  },
+  {
+    id: "erlang",
+    label: "erlang",
+    value: 416,
+    color: "hsl(320, 70%, 50%)",
+  },
+  {
+    id: "elixir",
+    label: "elixir",
+    value: 56,
+    color: "hsl(333, 70%, 50%)",
+  },
+  {
+    id: "c",
+    label: "c",
+    value: 192,
+    color: "hsl(91, 70%, 50%)",
+  },
+];
+
+const vaultData = [
+  {
+    id: "Required",
+    value: 99,
+    color: "hsl(87, 70%, 50%)",
+  },
+  {
+    id: "completed",
+    value: 490,
+    color: "hsl(173, 70%, 50%)",
+  }
+];
+
 
 const Dashboard = memo(({ details }) => {
   const { user, district } = useContext(AppContext);
 
   return (
-    <div className="pt-[7vh]">
-      <Image
-        src="/Assets/dash.jpg"
-        height={300}
-        width={500}
-        alt="Dashboard"
-        className="w-auto h-[50vh] object-cover"
-      />
-      <div className="p-4 ">
-        <h1 className="text-3xl font-bold py-6">
-          {dictionary[details]?.dashboard}
-        </h1>
+    <div className="flex flex-col">
+      <div className="pt-[7vh]">
+        <Image
+          src="/Assets/dash.jpg"
+          height={300}
+          width={500}
+          alt="Dashboard"
+          className="w-auto h-[50vh] object-cover"
+        />
+        <div className="p-4 ">
+          <h1 className="text-3xl font-bold py-6">
+            {dictionary[details]?.dashboard}
+          </h1>
 
-        <h3 className="text-lg">{dictionary[details]?.welcome}</h3>
-        <span className="text-2xl text-theme-blue font-bold">
-          {user?.fullName}
-        </span>
+          <h3 className="text-lg">{dictionary[details]?.welcome}</h3>
+          <span className="text-2xl text-theme-blue font-bold">
+            {user?.fullName}
+          </span>
+        </div>
+      </div>
+
+      <div className="pr-2 mb-4">
+        <h3 className="text-[1.5rem] pl-4 text-center">
+          Group Stats
+        </h3>
+        <PieChart data={data} />
+      </div>
+      <div className="pr-2 mb-4">
+        <h3 className="text-[1.5rem] pl-4 text-center">
+          Vault Stats
+        </h3>
+        <VaultChart data={vaultData} centerText={`${vaultData[0].value}%`}/>
       </div>
     </div>
   );
