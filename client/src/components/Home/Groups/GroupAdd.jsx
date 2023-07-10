657e2562cd0e0225987b86c8c1e94917996caeb2
Diptanshu Mahish <diptanshumahish2016@gmail.com>
2023-07-10 11:55:48 +0530

    Done

---
+++ client/src/components/Home/Groups/GroupAdd.jsx
@@ -0,0 +1,10 @@
+import { Plus } from "lucide-react";
+import React from "react";
+
+export default function GroupAdd() {
+  return (
+    <button className="fixed bg-theme-blue p-4 rounded-full text-white shadow-md bottom-[12vh] right-[10vw]">
+      <Plus />
+    </button>
+  );
+}
